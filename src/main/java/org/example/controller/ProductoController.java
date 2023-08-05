a1653ff67764e44d67f04f974220ab5bf2d16697
Calapzu <yamsoncalapzu@gmail.com>
2023-08-05 15:49:32 -0500

    DAO con INSERT del producto

--- src/main/java/org/example/controller/ProductoController.java
+++ src/main/java/org/example/controller/ProductoController.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 import org.example.factory.ConectionFactory;
 import org.example.modelo.Producto;
+import org.example.dao.ProductoDAO;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -104,74 +105,7 @@ public class ProductoController {
         int cantidad = producto.getCantidad();
         Integer maximaCantidad = 50;
         */
-        ConectionFactory factory = new ConectionFactory();
-        final Connection connection = factory.recuperaConexion();
-
-        try (connection) {
-
-            connection.setAutoCommit(false);
-
-            final PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO PRODUCTO (nombre, descripcion, cantidad)"
-                            + " VALUES (?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
-
-            try (preparedStatement) {
-                /*do {
-                    int cantidadParaGuardar = Math.min(cantidad, maximaCantidad);
-
-                    ejecutarRegistro(producto, preparedStatement);
-
-                    cantidad -= maximaCantidad;
-
-                } while (cantidad > 0);
-                */
-                ejecutarRegistro(producto, preparedStatement);
-                connection.commit();
-                System.out.println("COMMIT");
-            } catch (Exception e) {
-                connection.rollback();
-                System.out.println("ROLLBACK");
-            }
-
-        }
-    }
-
-
-    private void ejecutarRegistro(Producto producto, PreparedStatement preparedStatement) throws SQLException {
-
-        preparedStatement.setString(1, producto.getNombre());
-        preparedStatement.setString(2, producto.getDescripcion());
-        preparedStatement.setInt(3, producto.getCantidad());
-
-        preparedStatement.execute();
-
-        /**
-         * Try With Resources
-         * Con la version 7 de JAVA
-
-         try(ResultSet resultSet = preparedStatement.getGeneratedKeys()){
-         while (resultSet.next()) {
-         System.out.println(
-         String.format(
-         "Fue insertado en el producto de ID %d",
-         resultSet.getInt(1)));
-         }
-         }
-         * */
-
-        /**
-         * Try With Resources
-         * Con la version 9 de JAVA
-         */
-        final ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        try (resultSet) {
-            while (resultSet.next()) {
-                producto.setId(resultSet.getInt(1));
-                System.out.println(
-                        String.format(
-                                "Fue insertado en el producto de ID %s",
-                               producto));
-            }
-        }
+        ProductoDAO productoDAO = new ProductoDAO(new ConectionFactory().recuperaConexion());
+        productoDAO.guardar(producto);
     }
 }
